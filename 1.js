174c1093bb7b5f7412e56e913a428b06467d92e6
VwoLFy <v_wolfy@mail.ru>
2022-07-30 11:20:03 +0300

    1

---
+++ 1.js
@@ -0,0 +1,11 @@
+function countBy(x, n) {
+    let z = [];
+    let count = 0;
+    for (let i=1; count<n; i++) {
+      if (i%x==0) {
+        z.push (i);
+        count++;
+      }
+    }
+    return z;
+  }
