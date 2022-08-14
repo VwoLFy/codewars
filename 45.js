c4ed2eb41d0a8e97cb8000b02874d2841384b5ce
VwoLFy <v_wolfy@mail.ru>
2022-08-14 14:36:52 +0300

    45

--- 45.js
+++ 45.js
@@ -1,16 +1,12 @@
 function queueTime(customers, n) {
-    if (!customers.length) return 0;  
-    let tillsTime = [];
-    for (let till = 1; till <= n; till++) {
-        if (customers.length) tillsTime.push(customers.shift());
-    }
+    let tillsTime = new Array(n).fill(0);
 
     while (customers.length) {
-        tillsTime[tillsTime.indexOf(tillsTime.reduce((a, b) => a < b ? a : b))] 
-        += customers.shift();
+        tillsTime[tillsTime.indexOf(tillsTime.reduce((a, b) => a < b ? a : b))]
+            += customers.shift();
     };
 
-    return(tillsTime.reduce((a, b) => a > b ? a : b) );
+    return (tillsTime.reduce((a, b) => a > b ? a : b));
 }
 
-alert( queueTime([1,2,3,4,5], 100) );
+alert(queueTime([3, 4, 5, 3], 3));
